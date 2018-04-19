f798be9d3703328f17c2c36ac716caebc08f3ed4
jcristol <joshcristol@gmail.com>
2018-04-19 13:34:59 -0500

    forgot something again

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from distutils.core import setup
 
 setup(name='macattc',
       packages=['macattc'],
-      version='0.2',
+      version='0.3',
       description='get some free wifi boi',
       author='Josh Cristol',
       author_email='joshcristol@gmail.com',
       url='https://github.com/jcristol/macattc',
-      download_url = 'https://github.com/jcristol/macattc/archive/0.2.tar.gz', 
+      download_url = 'https://github.com/jcristol/macattc/archive/0.3.tar.gz', 
       install_requires=['tqdm', 'netifaces', 'netaddr', 'netifaces', 'wireless'],
       keyword=['hacking'],
       entry_points = {
